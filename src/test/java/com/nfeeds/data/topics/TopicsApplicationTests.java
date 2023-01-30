ca515c2b2e750cb9726ef2930bf2a6d206ac0bb6
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2023-01-30 15:30:20 +0100

    spring init

---
+++ src/test/java/com/nfeeds/data/topics/TopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfeeds.data.topics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
